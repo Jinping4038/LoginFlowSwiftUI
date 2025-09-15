08af937517bf61febcbc7e1774f9f3dc76683cc6
jzhang <jinping4038@gmail.com>
2025-09-15 14:05:37 -0700

    Initial Commit

---
+++ LoginWithSwiftUI/LoginWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithSwiftUIApp.swift
+//  LoginWithSwiftUI
+//
+//  Created by Jin Zhang on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginWithSwiftUITests/LoginWithSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithSwiftUITests.swift
+//  LoginWithSwiftUITests
+//
+//  Created by Jin Zhang on 9/15/25.
+//
+
+import Testing
+@testable import LoginWithSwiftUI
+
+struct LoginWithSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
